cd39cbf445ef1cb70815aa4ea26479cfe352e644
merg8511 <merg8511@gmail.com>
2024-06-04 23:32:14 -0600

    Agregar conexion a base de datos

--- API/Program.cs
+++ API/Program.cs
@@ -1,11 +1,21 @@
+using Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString)
+);
 
 var app = builder.Build();
 
