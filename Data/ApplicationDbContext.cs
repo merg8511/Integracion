2a081ff054f97cc66e5afc5d2f1650f267b55bc6
Carlos Piedra <barce90@hotmail.com>
2024-06-15 09:12:33 -0700

    Agregar Precio Promocional del Producto

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -15,5 +15,6 @@ namespace Data
         public DbSet<Categoria> Categorias { get; set; }
         public DbSet<Marca> Marcas { get; set; }
         public DbSet<Producto> Productos { get; set; }
+        public DbSet<PrecioOferta> PrecioOfertas { get; set; }
     }
 }
